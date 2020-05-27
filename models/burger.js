26d8a02d2b8064ddb76c56e28b948877c295831c
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-05-27 17:46:48 -0500

    able to work through router to generate list from sql

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,16 @@
-const orm = require("../config/orm")
+
+const connection = require("../config/connection");
+var express = require("express");
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    connection.query("SELECT * FROM burgers;", function (err, data) {
+        if (err) {
+            return res.status(500).end();
+        }
+        res.render('index', { burgers: data });
+    })
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,31 +1,30 @@
 // dependencies
 var express = require("express");
-var exphbs = require("express-handlebars");
-const orm = require("./config/orm")
-const connection = require("./config/connection");
-
-// create an instance of the express app.
-var app = express();
-// middleware to access the public file
-app.use(express.static("public"));
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var PORT = process.env.PORT || 8000;
 
+// create an instance of the express app.
+var app = express();
+
+// middleware to access the public file
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
 // set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.get('/', function (req, res) {
-    connection.query("SELECT * FROM burgers;", function (err, data) {
-        if (err) {
-            return res.status(500).end();
-        }
-        
-        res.render('index', { burgers: data });
-    })
-});
+// Import routes and give the server access to them.
+var routes = require("./models/burger");
+app.use(routes);
 
 app.listen(PORT, function () {
     // log (server-side) when our server has started

d3da4783331c6435949a92edede03cca3a77455a
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-05-26 22:42:25 -0500

    generating main page with express server and handelbars

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-let express = require("express")
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+// Create an instance of the express app.
+var app = express();
+// middleware to access the public file
+app.use(express.static("public"));
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  

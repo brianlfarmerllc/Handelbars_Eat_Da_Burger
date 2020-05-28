2762ada751104ac73e1a8eb4bb0998ec2e5db806
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-05-28 12:52:38 -0500

    commiting to push to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 const util = require("util");
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db",
-});
+
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db",
+  });
+}
+
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burgers_db",
+// });
 
 // using connection to connect
 connection.connect(function (err) {

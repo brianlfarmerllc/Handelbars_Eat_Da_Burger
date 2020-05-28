ee18ff5406c105c6013f2328f0d1e37764b5d92f
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-05-27 22:39:21 -0500

    able to delete burger from rendered list

--- models/burger.js
+++ models/burger.js
@@ -27,4 +27,20 @@ router.post("/api/burgers", function (req, res) {
     );
   });
 
+router.delete("/api/burgers/:id",function(req, res){
+  console.log(req.params.id)
+  connection.query(
+    "DELETE FROM burgers WHERE id = ?", [req.params.id], function (err, result) {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      } else if (result.affectedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+})
+
 module.exports = router;
